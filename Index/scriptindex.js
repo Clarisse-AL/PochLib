b1957d6f67656ad04e9d6c43876e5246cad82b7f
clarisse <clarissealamelu@gmail.com>
2021-08-25 18:57:19 +0200

    javascript btn ok

--- Index/scriptindex.js
+++ Index/scriptindex.js
@@ -1,11 +1,58 @@
-const form = document.getElementsByClassName('form');
-const newBook = document.getElementsByClassName('newBook');
 
-newBook.addEventListener('submit',function showForm(){
+// ****************AFFICHER-MASQUER FORMULAIRE*****************//
+const form = document.querySelector('.form');
+const newBook = document.querySelector('.newBook');
 
-    if (form.style.display = 'none') {
+const btnNewBook = document.querySelector('.btnNewBook');
+const btnCancel = document.querySelector('.btnCancel')
+
+//afficher formulaire
+btnNewBook.addEventListener('click', function showForm() {
         form.style.display = 'block';
         newBook.style.display = 'none';
-    }
 });
+
+//revenir à l'accueil - masquer formulaire
+btnCancel.addEventListener('click', function cancel() {
+        form.style.display = 'none';
+        newBook.style.display = 'block';
+});
+
+
+//*********ACCES API*********/
+
+const btnSearchBooks = document.querySelector('.btnSearchBooks');
+const apiGoogleBooks = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
+
+let bookList = document.querySelector('.bookList');
+let valueAuthor = document.querySelector('.valueAuthor');
+let valueTitle = document.querySelector('.valueTitle');
+
+
+
+valueTitle.addEventListener('input',
+        fetch(apiGoogleBooks, function (response) {
+                console.log(response)
+        })
+                .then(function (res) {
+                        if (res.ok) {
+                                return res.json();
+                        }
+                })
+                .then(function (data) {
+                        console.log(data);
+                        bookList.innerHTML =
+                                `<h1>Résultat de la recherche</h1>
+                                <ul>
+                                        <li>Auteur : ${data.items[""].volumeInfo.authors}</li >
+                                        <li>Titre : ${data.items[""].volumeInfo.title}</li>
+                                </ul > `;
+
+                        
+                })
+                .catch(function (err) {
+
+                })
+);
+
 
