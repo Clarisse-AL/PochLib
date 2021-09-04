ec3763f24d854f81bdce78618bf70ec08eecec28
clarisse <clarissealamelu@gmail.com>
2021-09-04 19:25:21 +0200

    bookcard ok

--- Index/scriptindex.js
+++ Index/scriptindex.js
@@ -16,7 +16,19 @@ btnNewBook.addEventListener('click', function showForm() {
 btnCancel.addEventListener('click', function cancel() {
         form.style.display = 'none';
         newBook.style.display = 'block';
+        window.location.reload();
 });
+
+//********changement d'icone click ********/
+// const iconFavorite = document.querySelector('.iconFavorite');
+
+// iconFavorite.addEventListener('click', function save(e) {
+//         e.preventDefault();
+//         iconFavorite.style.boxShadow = "10px 5px 5px grey";
+
+// });
+
+// save();
 
 
 //*********ACCES API*********/
@@ -25,22 +37,16 @@ const btnSearchBooks = document.querySelector('.btnSearchBooks');
 
 btnSearchBooks.addEventListener('click', function (e) {
         e.preventDefault();
-        console.log("ok");
 
         const fetchBook = () => {
-                let title = document.querySelector('.valueTitle').value;
-                console.log(title);
-
-                let authors = document.querySelector('.valueAuthor').value;
-                console.log(authors);
+                let valueTitle = document.querySelector('.valueTitle').value;
+                let valueAuthors = document.querySelector('.valueAuthor').value;
 
                 const apiGoogleBooks = `https://www.googleapis.com/books/v1/volumes?q=`
-                        + title
+                        + valueTitle
                         + '+inauthor:'
-                        + authors
+                        + valueAuthors
                         + '&key=AIzaSyBzPLXXa28wePRlPydq-cwJUNk1sP7W4Hg';
-                console.log(apiGoogleBooks);
-
 
                 fetch(apiGoogleBooks)
                         .then(function (res) {
@@ -50,9 +56,17 @@ btnSearchBooks.addEventListener('click', function (e) {
                         })
                         .then(function (data) {
                                 console.log(data)
-                                data.items.forEach((book) => {
-                                        createBook(book);
-                                });
+                                if (valueTitle == "" || valueTitle == undefined) {
+                                        alert("Merci de renseigner le titre du livre");
+                                }
+                                else if (data.totalItems === 0) {
+                                        alert("Aucun livre n’a été trouvé");
+                                }
+                                else {
+                                        data.items.forEach((book) => {
+                                                createBook(book);
+                                        });
+                                }
                         })
                         .catch(function (error) {
                                 console.error("erreur :" + error);
@@ -62,21 +76,46 @@ btnSearchBooks.addEventListener('click', function (e) {
         fetchBook();
 });
 
+//*****************Création des sections livres*************************//
 
-let booksList = document.querySelector('.booksList');
 
 function createBook(book) {
+        let booksList = document.querySelector('.booksList');
+        let bookCard = document.createElement('section');
+        let description = "";
+        let imgBook = "";
+        let authors = "";
 
-let bookCard = document.createElement('section');
+        if (book.volumeInfo.title)
 
-bookCard.innerHTML = `
-        <h3>${book.volumeInfo.title}</h3><br>
-        <h4>${book.volumeInfo.authors}</h4><br>
-        <p maxlength="200">${book.volumeInfo.description}</p><br>
-        <img src="${book.volumeInfo.imageLinks.thumbnail}">
+                if (book.volumeInfo.authors === undefined || book.volumeInfo.authors === null
+                        && book.volumeInfo.description === undefined || book.volumeInfo.description === null
+                        && book.volumeInfo.imageLinks === undefined || book.volumeInfo.imageLinks === null) {
+                        authors = "Information manquante";
+                        description = "Information manquante";
+                        imgBook = "image/unavailable.png";
+                } else {
+                        authors = book.volumeInfo.authors[0];
+                        description = book.volumeInfo.description.substring(0, 200);
+                        imgBook = book.volumeInfo.imageLinks.thumbnail;
+                };
+
+        bookCard.innerHTML = `
+        <h3 class="title">${book.volumeInfo.title}</h3>
+        <h4 class="authors">${authors}</h4>
+        <p class="idBook" >ID : ${book.id}<p>
+        <p class="description">${description}...</p><br>
+        <img class="imgBook" src="${imgBook}">
+        <div class="iconFavorite"><i class="fas fa-bookmark"></i></div>
         `;
 
-booksList.appendChild(bookCard);
-console.log(booksList);
+        booksList.appendChild(bookCard);
 
 };
+
+
+
+//*******trash*********//
+{/* <i class="fa-solid fa-trash"></i>
+<i class="fa-regular fa-trash"></i> */}
+
