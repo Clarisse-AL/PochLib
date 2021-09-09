2ee7b80da4d831f2870ebcabc1a03ea9eeecf6a1
clarisse <clarissealamelu@gmail.com>
2021-09-09 17:15:23 +0200

    storage not ok

--- scriptindex.js
+++ scriptindex.js
@@ -26,49 +26,53 @@ const btnSearchBooks = document.querySelector('.btnSearchBooks');
 
 btnSearchBooks.addEventListener('click', function (e) {
         e.preventDefault();
-
-        const fetchBook = () => {
-                let valueTitle = document.querySelector('.valueTitle').value;
-                let valueAuthors = document.querySelector('.valueAuthor').value;
-
-                const apiGoogleBooks = `https://www.googleapis.com/books/v1/volumes?q=`
-                        + valueTitle
-                        + '+inauthor:'
-                        + valueAuthors
-                        + '&key=AIzaSyBzPLXXa28wePRlPydq-cwJUNk1sP7W4Hg';
-
-                fetch(apiGoogleBooks)
-                        .then(function (res) {
-                                if (res.ok) {
-                                        return res.json();
-                                }
-                        })
-                        .then(function (data) {
-                                console.log(data)
-                                if (valueTitle == "" || valueTitle == undefined) {
-                                        alert("Merci de renseigner le titre du livre");
-                                }
-                                else if (data.totalItems === 0) {
-                                        alert("Aucun livre n’a été trouvé");
-                                }
-                                else {
-                                        data.items.forEach((book) => {
-                                                createBook(book);
-                                        });
-                                }
-                        })
-                        .catch(function (error) {
-                                console.error("erreur :" + error);
-
-                        })
-        }
         fetchBook();
 });
+const fetchBook = () => {
+        let valueTitle = document.querySelector('.valueTitle').value;
+        let valueAuthors = document.querySelector('.valueAuthor').value;
 
-//*****************Création des sections livres*************************//
+        const apiGoogleBooks = `https://www.googleapis.com/books/v1/volumes?q=`
+                + valueTitle
+                + '+inauthor:'
+                + valueAuthors
+                + '&key=AIzaSyBzPLXXa28wePRlPydq-cwJUNk1sP7W4Hg'
+                ;
+
+        fetch(apiGoogleBooks)
+                .then(function (res) {
+                        if (res.ok) {
+                                return res.json();
+                        }
+                })
+                .then(function (data) {
+                        console.log(data)
+                        if (valueTitle == "" || valueTitle == undefined) {
+                                alert("Merci de renseigner le titre du livre");
+                        }
+                        else if (data.totalItems === 0) {
+                                alert("Aucun livre n’a été trouvé");
+                        }
+                        else {
+                                data.items.forEach((book) => {
+                                        createBook(book);
+                                });
+                        }
+                })
+                .catch(function (error) {
+                        console.error("erreur :" + error);
+
+                })
+};
+
+
+
+
 
 
 function createBook(book) {
+
+        //Création des sections livres pour le résultat de la recherche
         let booksList = document.querySelector('.booksList');
         let bookCard = document.createElement('section');
         let description = "";
@@ -91,7 +95,7 @@ function createBook(book) {
 
         bookCard.innerHTML = `
         <header>
-        <div class="iconFavorite"><i class="far fa-bookmark"></i></div>        
+        <div class="bookmark"><i class="far fa-bookmark"></i></div>        
         <h3 class="title">${book.volumeInfo.title}</h3>   
         <header>
         <h4 class="authors">${authors}</h4>
@@ -102,21 +106,56 @@ function createBook(book) {
 
         booksList.appendChild(bookCard);
 
+        // création des sections livres dans la pochlist
+
+        let bookmark = document.querySelector('.bookmark');
+        let bookshelf = document.querySelector('.bookshelf');
+        let idBook = document.querySelector('.idBook');
+        idBook.toString();
+        
+
+        bookmark.addEventListener('click', function () {
+
+                // récupérer la clé
+                let getIdBook = localStorage.getItem('idBook');
+                console.log(getIdBook);
+
+                if (getIdBook != null) {
+                        bookCard.style.display = "none";
+                        setBooksInfo();
+                }
+
+                // Enregistrer l'ensemble de clé valeur dans la bibliothèque
+                function setBooksInfo() {
+
+                        setBooksInfo = localStorage.setItem("idBook", "book.volumeInfo.title");
+                        console.log(setBooksInfo);
+
+                        bookCard.innerHTML = `
+                                <header>
+                                <div class="trash"><i class="fad fa-trash"></i></div>        
+                                <h3 class="title">${book.volumeInfo.title}</h3>   
+                                <header>
+                                <h4 class="authors">${authors}</h4>
+                                <p class="idBook" >ID : ${book.id}<p>
+                                <p class="description">${description}...</p><br>
+                                <img class="imgBook" src="${imgBook}">
+                                `;
+                        bookCard.appendChild(bookshelf);
+                };
+        });
+
+        // supprimer le livre de la pochlist
+
+        let trash = document.querySelector('.trash');
+
+        trash.addEventListener('click', function trashBooks() {
+                localStorage.removeItem();
+        })
+
 };
 
 
-//*****Ajout dans les favoris *********//
-let iconFavorite = document.querySelector('.header');
-console.log(iconFavorite);
-
-iconFavorite.addEventListener('click', function addPochlist(e){
-        e.preventDefault();
-
-
-        console.log("ok");
-        iconFavorite.innerHTML = `<i class="fas fa-bookmark"></i>`;
-        alert("Ce livre à correctement été ajouté dans votre Poch'List");
-});
 
 
 
@@ -125,7 +164,11 @@ iconFavorite.addEventListener('click', function addPochlist(e){
 
 
 
-//*******trash*********//
-{/* <i class="fa-solid fa-trash"></i>
-<i class="fa-regular fa-trash"></i> */}
+
+
+
+
+
+
+
 
